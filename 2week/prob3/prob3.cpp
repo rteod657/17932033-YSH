c651c6e155cd7acaab9d70cb82da0bebff09582b
rteod657 <70637167+rteod657@users.noreply.github.com>
2022-09-21 11:31:21 +0900

    2week-prob3
    변수
    학점은 소수점이 없어 int로 사용
    평점과 평균은 소수점이 있어 double를 사용
    
    각각의 계산을 넣어놓고
    credits는 10이상, grade는 4 초과로 정함
    
    결과
    평균이 4 보다 크면 참 1, 그렇지 않으면 0

---
+++ 2week/prob3/prob3.cpp
@@ -0,0 +1,16 @@
+#include<stdio.h>
+
+int main(void)
+{
+	int kor = 3, eng = 5, mat = 4;
+	int credits;
+	int res;
+	double kscore = 3.8, escore = 4.4, mscore = 3.9;
+	double grade;
+
+	credits = kor + eng + mat;
+	grade = (kor * kscore + eng * escore + mat * mscore);
+	res = (credits >= 10) && (grade > 4);
+	printf("%d\n", res);
+	return 0;
+}
