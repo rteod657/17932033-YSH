f1e26edb5573aef2d855066b84261e9473c3ba0d
rteod657 <70637167+rteod657@users.noreply.github.com>
2022-10-05 12:56:19 +0900

    5week

--- 5week/prob2/2.cpp
+++ 5week/prob2/2.cpp
@@ -2,34 +2,38 @@
 
 int main(void)
 {
-    int integer;
-    int i, j;
-    int pointer;
-    int count = 0;
-
-    printf("Please enter an integer greater than or equal to 2 : ");
-    scanf("%d", &integer);
-
-    for (i = 2; i <= integer; i++)
+    while (1)
     {
-        pointer = 1;
-        for (j = 2; j < i; j++)
+        int integer;
+        int i, j;
+        int pointer;
+        int count = 0;
+
+        printf("\nEnter 0 to exit \n");
+        printf("Please enter an integer greater than or equal to 2 : ");
+        scanf_s("%d", &integer);
+
+        for (i = 2; i <= integer; i++)
         {
-            if ((i % j) == 0)
+            pointer = 1;
+            for (j = 2; j < i; j++)
             {
-                pointer = 0;
-                break;
+                if ((i % j) == 0)
+                {
+                    pointer = 0;
+                    break;
+                }
+            }
+            if (pointer == 1)
+            {
+                printf("%5d", i);
+                count++;
+                if ((count % 5) == 0)
+                {
+                    printf("\n");
+                }
             }
         }
-        if (pointer == 1)
-        {
-            printf("%5d", i);
-            count++;
-            if ((count % 5) == 0)
-            {
-                printf("\n");
-            }
-        }
+        if (integer == 0) break;
     }
-    return 0;
 }
