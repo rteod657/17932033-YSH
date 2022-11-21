b563a754b7b0f878fcaa02645da7e6cb8b34e651
rteod657 <70637167+rteod657@users.noreply.github.com>
2022-11-22 01:28:08 +0900

    474

---
+++ 11week/11week/p474/474.cpp
@@ -0,0 +1,54 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+void saveData(int* nInteger, int nMax);
+void printData(int* nInteger, int nMax);
+
+int main(void)
+{
+  int* nInteger;
+  int nMax;
+  printf("정수를 입력하세요 : ");
+  scanf_s("%d", &nMax);
+  nInteger = (int*)calloc(nMax, sizeof(int));
+  saveData(nInteger, nMax);
+  printData(nInteger, nMax);
+  return 0;
+}
+
+void saveData(int* nInteger, int nMax)
+{
+  int i, k;
+  for (i = 0; i < nMax; i++)
+  {
+    nInteger[i] = 0;
+  }
+  for (i = 2; i < nMax; i++)
+  {
+    for (k = 2; k < i; k++)
+    {
+      if (i % k == 0) break;
+    }
+    if (i == k)
+    {
+      nInteger[i - 2] = i; // i가 2부터 시작하기 때문에, i-2
+    }
+  }
+}
+
+void printData(int* nInteger, int nMax)
+{
+  int i;
+  for (i = 0; i < nMax; i++)
+  {
+    if (nInteger[i] == 0)
+    {
+      printf("X\t");
+    }
+    else
+    {
+      printf("%d\t", nInteger[i]);
+    }
+    if ((i + 1) % 5 == 0) printf("\n");
+  }
+}
